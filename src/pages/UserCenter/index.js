329ac22722bb7550029b55985a744f72acc9dcef
MastyJay <mastyjay@163.com>
2019-08-30 00:22:52 +0800

    三个测试失败

--- src/pages/UserCenter/index.js
+++ src/pages/UserCenter/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import styles from './index.css';
+import React, { Fragment } from 'react';
+import UserCenter from './UserCenter';
 
 export default function() {
   return (
-    <div className={styles.normal}>
-      <h1>I am a umi block!</h1>
-    </div>
+    <Fragment>
+      <UserCenter />
+    </Fragment>
   );
 }
